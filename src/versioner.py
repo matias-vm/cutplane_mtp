85382ca50331122a579ce20bd84ffc31eab6465e
matias <mvillagra89@gmail.com>
2024-06-17 16:45:45 -0400

    adding versioner.py

---
+++ src/versioner.py
@@ -0,0 +1,23 @@
+###############################################################################
+#
+# This code was written by Daniel Bienstock and is being maintained 
+# by Matias Villagra.
+# 
+# Please report any bugs or issues (for sure there will be) to
+#                         mjv2153@columbia.edu
+#
+# Oct 2023
+###############################################################################
+
+# -*- coding: utf-8 -*-
+"""
+
+""" 
+import sys
+import time
+
+from log import danoLogger
+
+def stateversion(log):
+    log.joint("Version <Tue.Sep.26.015044.2023@frito.local>\n")
+
